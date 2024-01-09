ca427af77a610d38af43186d6c73560e02748f63
rafail <rafail.dev@gmail.com>
2024-01-09 22:47:47 +0000

    collatz refactor

--- module-1/8 collatz.hs
+++ module-1/8 collatz.hs
@@ -7,12 +7,6 @@ collatzStepCount n =
 collatzList 1 = [1]
 collatzList 2 = [2, 1]
 collatzList 3 = [3, 10, 5, 16, 8, 4, 2, 1]
-collatzList n = generate n []
-  where
-    generate 1 list = list ++ [1]
-    generate n list = generate nextN (list ++ [n])
-      where
-        nextN =
-          if even n
-            then n `div` 2
-            else n * 3 + 1
+collatzList n
+  | even n = n : collatzList (n `div` 2)
+  | otherwise = n : collatzList (n * 3 + 1)
