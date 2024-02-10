66430636de5c69cedf5933c22a71c4b4fa5ac4c1
rafail-dev <rafail.dev@gmail.com>
2024-02-10 10:39:15 +0000

    median

--- module-3/20 time series.hs
+++ module-3/20 time series.hs
@@ -1,4 +1,5 @@
 import Data.Array
+import Data.List (sort)
 import Data.Map qualified as Map
 import Data.Maybe (catMaybes, fromJust, isJust)
 import Data.Semigroup ()
@@ -123,6 +124,7 @@ mconcat $ map fileToTS [file1, file2, file3, file4]
 -- ==
 
 mean :: (Real a) => [a] -> Double
+mean [] = error "empty array"
 mean xs = total / count
   where
     total = (realToFrac . sum) xs
@@ -194,6 +196,25 @@ movingAvgTS (TS times values) n = TS times smoothedValues
 
 -- ==
 
+median :: (Real a) => [a] -> Double
+median [] = error "empty array"
+median xs = realToFrac doubleMedian / 2
+  where
+    sorted = sort xs
+    firstCenter = length xs `div` 2
+    center
+      | odd $ length xs = [firstCenter, firstCenter]
+      | otherwise = [firstCenter, firstCenter - 1]
+    doubleMedian = sum (map (sorted !!) center)
+
+medianTS :: (Real a) => TS a -> Maybe Double
+medianTS (TS _ []) = Nothing
+medianTS (TS _ values)
+  | all (== Nothing) values = Nothing
+  | otherwise = Just $ median $ catMaybes values
+
+-- ==
+
 main :: IO ()
 main = do
   let tsAll = mconcat $ map fileToTS [file1, file2, file3, file4]
@@ -219,4 +240,8 @@ main = do
 
   putStrLn "3-point moving Avg"
   print $ movingAvgTS tsAll 3
+  putStrLn ""
+
+  putStrLn "Median"
+  print $ medianTS tsAll
   putStrLn ""
