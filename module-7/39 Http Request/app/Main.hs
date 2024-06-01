2146565b7ddb94d80bb91a6eaafd4a24ea2c370a
rafail <rafail.dev@gmail.com>
2024-06-01 06:26:21 +0000

    Homework 39.2

--- module-7/39 Http Request/app/Main.hs
+++ module-7/39 Http Request/app/Main.hs
@@ -2,16 +2,20 @@ module Main (main) where
 
 import qualified Data.ByteString.Lazy as BL
 import Lib (Method (..), buildRequest)
-import Network.HTTP.Simple (getResponseBody, getResponseStatusCode, httpLBS)
+import Network.HTTP.Simple (getResponseBody, getResponseStatus, getResponseStatusCode, httpLBS)
+import Network.HTTP.Types.Status (statusMessage)
 
 main :: IO ()
 main = do
   response <- httpLBS (buildRequest GET "www.ncdc.noaa.gov" "/cdo-web/api/v2/datasets" "<TOKEN from https://github.com/NOAAGov>")
-
-  if getResponseStatusCode response == 200
+  let statusCode = getResponseStatusCode response
+  let jsonBody = getResponseBody response
+  if statusCode == 200
     then do
       putStrLn "Результаты запроса были сохранены в файл"
-      let jsonBody = getResponseBody response
       BL.writeFile "data.json" jsonBody
-    else
+    else do
+      -- print $ statusMessage $ getResponseStatus response -- statusMessage is " "
+      print jsonBody -- error message is here
+      print statusCode
       putStrLn "Запрос не удалось совершить из-за ошибки"
